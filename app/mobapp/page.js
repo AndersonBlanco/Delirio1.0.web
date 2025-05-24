afced1107fcccd18902dbf4da0246f417172e360
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2025-05-24 14:40:06 -0400

    app-video

--- app/mobapp/page.js
+++ app/mobapp/page.js
@@ -46,16 +46,20 @@ export default function MobApp(){
         <div style= {{minWidth: "170px", maxWidth: "270px"}}>    
             <Image   layout = {"responsive"}  height={100} style= {{}} src = {U2}/>
                 </div>
- <div style= {{minWidth: "200px", maxWidth: "300px"}}>
+ <div style= {{minWidth: "200px", maxWidth: "300px", backgroundColor: "transparent"}}>
 
 <Image layout = {"responsive"}  height={100} style= {{}} src = {U3}/>
 </div>    
 
  
-  
- <div className={styles.iframe1_container} style = {{backgroundColor:"transparent", overflow: "hidden", minWidth: "200px", maxWidth: "300px", width: "10vw", overflowY:"hidden",justifyContent:"center", alignItems:"center", height: "42vw", width:"50vw", overflow:"hidden", position:"relative"}}>
-    <iframe allowtransparency className={styles.iframe1} style = {{borderWidth:0, backgroundColor: "transparent", position:"relative", left: "-130%",}} src="https://drive.google.com/file/d/1qudC8qPXAyvBLSN7JfD6PZrzAws5h2EM/preview" allow="autoplay" title="FiTech App"/>
-    </div>
+
+
+ 
+ <div className={styles.iframe1_container} style = {{backgroundColor:"transparent", overflow: "hidden", minWidth: "200px", maxWidth: "300px", width: "10vw", overflowY:"hidden",justifyContent:"center", alignItems:"center", height: "61vh", width:"50vw", overflow:"hidden", position:"relative"}}>
+
+            <iframe allowtransparency className={styles.iframe1} style = {{borderWidth:0, backgroundColor: "transparent", position:"absolute", left: "-130%",}} src="https://drive.google.com/file/d/1qudC8qPXAyvBLSN7JfD6PZrzAws5h2EM/preview" allow="autoplay" title="FiTech App"/>
+
+ </div>
      
  
 <div style= {{minWidth: "170px", maxWidth: "270px"}}>
